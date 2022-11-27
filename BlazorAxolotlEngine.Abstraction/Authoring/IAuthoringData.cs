93028071dc221f9827cd9afff78cbcd7adc3330f
Alexis C. Gridel <contact@agdl.dev>
2022-11-27 11:53:38 +0100

    feat(ecs): base code around component data

---
+++ BlazorAxolotlEngine.Abstraction/Authoring/IAuthoringData.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace BlazorAxolotlEngine.Abstraction.Authoring;
+
+public interface IAuthoringData
+{
+    
+}
---
+++ BlazorAxolotlEngine.Abstraction/Entity/IEntity.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace BlazorAxolotlEngine.Abstraction;
+
+public interface IEntity
+{
+    public void OnCreate();
+    public void OnDestroy();
+    public void OnUpdate();
+}
