b640836bec6d09890dd64b570436353b4336ec70
Alexis C. Gridel <contact@agdl.dev>
2022-11-28 11:44:52 +0100

    feat(component): transform initial component

---
+++ src/BlazorAxolotlEngine.Component/Transform.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using System.Numerics;
+using BlazorAxolotlEngine.Abstraction.Component;
+
+namespace BlazorAxolotlEngine.Component;
+
+public struct Transform 
+    : IComponentData
+{
+    Vector3 _position;
+    Quaternion _rotation;
+}
